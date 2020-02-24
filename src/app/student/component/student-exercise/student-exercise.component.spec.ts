33d3f1143f07efe9da6a161876ecba1995c921ab
HongyiLU <adamlu389535891@gmail.com>
2020-02-24 15:38:10 +0100

    Add exercise module

--- src/app/student/component/student-exercise/student-exercise.component.spec.ts
+++ src/app/student/component/student-exercise/student-exercise.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { StudentExerciseComponent } from './student-exercise.component';
 
@@ -13,7 +10,7 @@ describe('StudentExerciseComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ StudentExerciseComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
